50a7a61b602e2df93c4f54642e64356faa74044e
unknown <amincxo@gmail.com>
2025-01-22 16:26:17 +0330

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
+import BlogTableComponent from './components/BlogTables.tsx';
+import BlogCardComponent from './components/BlogCard.tsx';
+import NavBar from './components/Navbar.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+                <NavBar />
+                <BlogTableComponent/>
+                <BlogCardComponent/>
+        </header>
     </div>
   );
 }
