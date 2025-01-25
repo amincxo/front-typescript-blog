d6133399938a0d88db430d2f7bbec47e576a71c1
unknown <amincxo@gmail.com>
2025-01-25 15:34:58 +0330

    asdf

---
+++ src/components/BlogList.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React , {useState} from 'react'
+import { IBlog } from '../types/blog.type';
+//import { FC } from "react";
+//import { PropsBlog } from "../types/blog.types";
+const BlogCardComponent = () => {
+    const [blog , setBlogs] = useState<IBlog[]>([]);
+    setBlogs([
+        {
+            title: 'react tailwaind card with Grid1',
+            author: 'aminBorvayeh',
+            image: 'https://picsum.photos/200/300',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum'
+        },
+        {
+            title: 'react tailwaind card with Grid1',
+            author: 'aminBorvayeh',
+            image: 'https://picsum.photos/200/300',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum'
+        },
+        {
+            title: 'react tailwaind card with Grid1',
+            author: 'aminBorvayeh',
+            image: 'https://picsum.photos/200/300',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum'
+        },
+        {
+            title: 'react tailwaind card with Grid1',
+            author: 'aminBorvayeh',
+            image: 'https://picsum.photos/200/300',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum'
+        }
+    ])
+    return (
+        <>
+            <div className="w-full shadow-md lg:max-w-sm p-2">
+                <img
+                    className="object-cover w-full h-48"
+                    src="https://picsum.photos/536/354"
+                    alt="image"
+                />
+                <div className="p-4">
+                    <h4 className="text-xl font-semibold text-blue-600">
+
+                        {/* {blog.title} */}
+                    </h4>
+                    <p className="mb-2 leading-normal">
+                        {/* {blog.text} */}
+                    </p>
+                    <button className="px-2 py-2 text-sm text-black-300">
+                        {/* {blog.author} */}
+                    </button>
+                    <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
+                        Read more
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+}
+export default BlogCardComponent
